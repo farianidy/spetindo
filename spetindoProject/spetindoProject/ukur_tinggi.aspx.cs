09038abef1c5660b2784239d27cb38ae77f705e1
eka.ayu.p@gmail.com <eka.ayu.p@gmail.com@cf2be681-e813-03c8-39af-820890261f47>
2013-04-22 08:38:56 +0000

    update dikit mod_pempembudi info_pempembudi ukur_suhu ukur_tinggi

--- spetindoProject/spetindoProject/ukur_tinggi.aspx.cs
+++ spetindoProject/spetindoProject/ukur_tinggi.aspx.cs
@@ -11,7 +11,7 @@ namespace spetindoProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Page.Title = "Cara Mengukur Ketinggian Tanah";
         }
     }
 }
