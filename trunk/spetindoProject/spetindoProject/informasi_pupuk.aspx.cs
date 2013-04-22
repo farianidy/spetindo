b2720ef379ebc98c9ad8a177a5181226e6b635e4
petiteaiueo@gmail.com <petiteaiueo@gmail.com@cf2be681-e813-03c8-39af-820890261f47>
2013-04-22 22:30:58 +0000

    Tabel + Controller di: - informasi_hama - informasi_pupuk

--- trunk/spetindoProject/spetindoProject/informasi_pupuk.aspx.cs
+++ trunk/spetindoProject/spetindoProject/informasi_pupuk.aspx.cs
@@ -76,5 +76,209 @@ namespace spetindoProject
             }
 
         }
+
+        #region PupukHandler
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            RefreshGridView1();
+        }
+
+        /* To reload gridview with sort and keyword key */
+        private void RefreshGridView1()
+        {
+            DataTable dt = null;
+
+            string id_tanaman = "";
+
+            id_tanaman = DropDownListTanaman.Text;
+
+            OracleConnection connect = new OracleConnection();
+            OracleDataReader reader;
+
+            string strconnect = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            connect.ConnectionString = strconnect;
+            string strquery = "select nama_pupuk, harga_pupuk, takaran, nama_jenis_pupuk from tanaman a, detail_pupuk b, pupuk c, jenis_pupuk d where a.id_tanaman='" + id_tanaman + "'and a.id_tanaman= b.id_tanaman and b.id_pupuk= c.id_pupuk and c.id_jenis_pupuk= d.id_jenis_pupuk";
+            OracleCommand command = new OracleCommand(strquery, connect);
+
+            try
+            {
+                connect.Open();
+                reader = command.ExecuteReader();
+                dt.Load(reader);
+
+                reader.Close();
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+            if (ViewState["SortExpression"] != null)
+                dt.DefaultView.Sort = ViewState["SortExpression"].ToString() + " " + ViewState["SortDirection"].ToString();
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            ViewState["SortExpression"] = e.SortExpression;
+
+            if (ViewState["SortDirection"] == null)
+                ViewState["SortDirection"] = "desc";
+            else
+            {
+                if (ViewState["SortDirection"].ToString() == "asc")
+                    ViewState["SortDirection"] = "desc";
+                else
+                    ViewState["SortDirection"] = "asc";
+            }
+
+            RefreshGridView1();
+        }
+
+        /* Handle the paging of gridview. */
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            RefreshGridView1();
+        }
+        #endregion
+
+        #region HaraHandler
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+            RefreshGridViewHaraP();
+            RefreshGridViewHaraK();
+        }
+
+        /* To reload gridview with sort and keyword key */
+        private void RefreshGridViewHaraP()
+        {
+            DataTable dt = null;
+
+            namaprovinsi = ListProvinsi.SelectedValue;
+            namakota = ListKota.SelectedValue;
+            ListProvinsi.Items.Clear();
+            isiListProvinsi();
+
+            string strconnect = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            connect.ConnectionString = strconnect;
+
+            Response.Write(namaprovinsi);
+            Response.Write(namakota);
+
+            string strquery = "select DISTINCT nama_provinsi, nama_kota, nama_status, luas_lahan, kebutuhan_pupuk from provinsi a, kota b, status_hara c, ukuran d where a.id_provinsi= b.id_provinsi and a.nama_provinsi = '" + namaprovinsi + "' and b.id_kota= d.id_kota and b.nama_kota='" + namakota + "' and c.id_status= d.id_status and d.id_pupuk='PU003'";
+            OracleCommand command = new OracleCommand(strquery, connect);
+
+            try
+            {
+                connect.Open();
+                reader = command.ExecuteReader();
+                dt.Load(reader);
+
+                reader.Close();
+
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+            if (ViewState["SortExpression"] != null)
+                dt.DefaultView.Sort = ViewState["SortExpression"].ToString() + " " + ViewState["SortDirection"].ToString();
+
+            GridViewHaraP.DataSource = dt;
+            GridViewHaraP.DataBind();
+        }
+
+        protected void GridViewHaraP_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            ViewState["SortExpression"] = e.SortExpression;
+
+            if (ViewState["SortDirection"] == null)
+                ViewState["SortDirection"] = "desc";
+            else
+            {
+                if (ViewState["SortDirection"].ToString() == "asc")
+                    ViewState["SortDirection"] = "desc";
+                else
+                    ViewState["SortDirection"] = "asc";
+            }
+
+            RefreshGridViewHaraP();
+        }
+
+        /* Handle the paging of gridview. */
+        protected void GridViewHaraP_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewHaraP.PageIndex = e.NewPageIndex;
+            RefreshGridViewHaraP();
+        }
+
+        /* To reload gridview with sort and keyword key */
+        private void RefreshGridViewHaraK()
+        {
+            DataTable dt = null;
+
+            namaprovinsi = ListProvinsi.SelectedValue;
+            namakota = ListKota.SelectedValue;
+            ListProvinsi.Items.Clear();
+            isiListProvinsi();
+
+            string strconnect = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            connect.ConnectionString = strconnect;
+
+            Response.Write(namaprovinsi);
+            Response.Write(namakota);
+
+            string strquery2 = "select DISTINCT nama_provinsi, nama_kota, nama_status, luas_lahan, kebutuhan_pupuk from provinsi a, kota b, status_hara c, ukuran d where a.id_provinsi= b.id_provinsi and a.nama_provinsi = '" + namaprovinsi + "' and b.id_kota= d.id_kota and b.nama_kota='" + namakota + "' and c.id_status= d.id_status and d.id_pupuk='PU005'";
+            OracleCommand command2 = new OracleCommand(strquery2, connect);
+
+            try
+            {
+                connect.Open();
+                reader2 = command2.ExecuteReader();
+                dt.Load(reader2);
+
+                reader2.Close();
+
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+            if (ViewState["SortExpression"] != null)
+                dt.DefaultView.Sort = ViewState["SortExpression"].ToString() + " " + ViewState["SortDirection"].ToString();
+
+            GridViewHaraK.DataSource = dt;
+            GridViewHaraK.DataBind();
+        }
+
+        protected void GridViewHaraK_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            ViewState["SortExpression"] = e.SortExpression;
+
+            if (ViewState["SortDirection"] == null)
+                ViewState["SortDirection"] = "desc";
+            else
+            {
+                if (ViewState["SortDirection"].ToString() == "asc")
+                    ViewState["SortDirection"] = "desc";
+                else
+                    ViewState["SortDirection"] = "asc";
+            }
+
+            RefreshGridViewHaraK();
+        }
+
+        /* Handle the paging of gridview. */
+        protected void GridViewHaraK_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewHaraK.PageIndex = e.NewPageIndex;
+            RefreshGridViewHaraK();
+        }
+        #endregion
     }
 }
