9ab3d06585cc4fe21c1d4f09e2d3fc09db0b33de
eka.ayu.p@gmail.com <eka.ayu.p@gmail.com@cf2be681-e813-03c8-39af-820890261f47>
2013-05-02 12:10:38 +0000

    perbaikan modul nasihat

---
+++ spetindoProject/spetindoProject/forecast.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace spetindoProject
+{
+    public class forecast
+    {
+        public double[,] harga = new double[3,1000000];
+        public int k, i = 0, batas = 12, n = 0;
+        double avrg;
+        public double[] output = new double[100000];
+
+
+        public double ramalPadi()
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                double sum = 0;
+                for (; i < batas; i++)
+                {
+                    sum = sum + harga[0,i];
+                }
+
+                avrg = sum / 12;
+                harga[0,i + 1] = avrg;
+                output[n] = avrg;
+                n++;
+                batas++;
+                i = n;
+
+            }
+                    return (output[5]);
+        }
+
+        public double ramalJagung()
+        {
+            n = 0;
+            for (int j = 0; j < 6; j++)
+            {
+                double sum = 0;
+                for (; i < batas; i++)
+                {
+                    sum = sum + harga[1, i];
+                }
+
+                avrg = sum / 12;
+                harga[1, i + 1] = avrg;
+                output[n] = avrg;
+                n++;
+                batas++;
+                i = n;
+
+            }
+            return (output[5]);
+        }
+
+        public double ramalKedelai()
+        {
+            n = 0;
+            for (int j = 0; j < 6; j++)
+            {
+                double sum = 0;
+                for (; i < batas; i++)
+                {
+                    sum = sum + harga[2, i];
+                }
+
+                avrg = sum / 12;
+                harga[2, i + 1] = avrg;
+                output[n] = avrg;
+                n++;
+                batas++;
+                i = n;
+
+            }
+            return (output[5]);
+        }
+
+    }
+}
